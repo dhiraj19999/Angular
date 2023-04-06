f1786bd54fbe20d3b066fa0b9af808e0f53193a2
dhiraj19999 <yogeshsamse682@gmail.com>
2023-04-06 18:07:25 +0530

    add

---
+++ Day1/myApp/src/app/man-list/man-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-man-list',
+  template: `
+    <h2 class="cus">
+      tepmlate or inline 
+    </h2>
+  `,
+  styles: [
+  `.cus{color:red}`
+  ]
+})
+export class ManListComponent {
+
+}
---
+++ Day1/myApp/src/app/user-list/user-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styles: [
+    `.inline{color:blue}`
+  ]
+})
+export class UserListComponent {
+
+}
